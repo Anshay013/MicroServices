e07544b38eaa757b5edd85b2b6d1038f0f06cc9a
Anshay013 <anshay013dbz@gmail.com>
2025-04-27 22:47:19 +0530

    user service 2

--- src/main/java/com/sasuke/user/service/UserService/UserServiceApplication.java
+++ src/main/java/com/sasuke/user/service/UserService/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.sasuke.user.service.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(scanBasePackages = "com.sasuke.user.service")
+@SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
